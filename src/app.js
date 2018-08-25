57a4b6fd889c5b9bd6040efbacc0de6dc97f22f2
Oluwaremi Oluwole <polymathwole@gmail.com>
2018-08-25 13:09:18 +0100

    Basic Express app

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import logger from 'morgan';
+import routes from './routes';
+
+const app = express();
+
+app.set('port', parseInt(process.env.PORT, 10) || 8080);
+app.disable('x-powered-by');
+
+app.use(logger('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+routes(app);
+
+app.get('/', (req, res) => res.status(200).json({ message: 'Welcome!' }));
+
+app.all('*', (req, res) => res.status(404).json({ message: 'Page not found!' }));
+
+app.listen(app.get('port'), () => console.log(`App listening on ${app.get('port')}`));
+
+export default app;
---
+++ src/routes/index.js
@@ -0,0 +1,5 @@
+const router = (app) => {
+
+};
+
+export default router;
