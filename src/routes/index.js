c9f2fdb9b79f59f5b1e058e0dc3507be95b1b67e
Oluwaremi Oluwole <polymathwole@gmail.com>
2018-08-26 22:26:31 +0100

    Set up Swagger

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@ import auth from '../middlewares/auth';
 import controllers from '../controllers';
 
 const router = (app) => {
-  app.post('/api/v1/login', auth.loginAuth, controllers.user.login);
-  app.patch('/api/v1/patchjson', auth.allowAccess, controllers.user.patchjson);
+  app.post('/api/v1/user/login', auth.loginAuth, controllers.user.login);
+  app.patch('/api/v1/user/patchjson', auth.allowAccess, controllers.user.patchjson);
 };
 
 export default router;
