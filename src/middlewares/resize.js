f3a041aedabf3f50f2bd387c610773bebd72a316
Oluwaremi Oluwole <polymathwole@gmail.com>
2018-08-28 14:34:15 +0100

    Add Network validation to generate thumbnail

--- src/middlewares/resize.js
+++ src/middlewares/resize.js
@@ -1,10 +1,12 @@
 import jimp from 'jimp';
 import path from 'path';
+import fs from 'fs';
 
 const resize = (req, res) => {
   const { url, format } = req.body;
   const imgnam = Date.now().toString();
   const formats = ['jpg', 'jpeg', 'png', 'bmp', 'tiff', 'gif'];
+  let imgPath = 'xxx';
   let rightFormat = 0;
 
 
@@ -24,12 +26,36 @@ const resize = (req, res) => {
         .resize(50, 50)
         .write(`image_${imgnam}_resized.${format}`);
 
-      const imgPath = path.join(process.cwd(), `image_${imgnam}_resized.${format}`);
+      imgPath = path.join(process.cwd(), `image_${imgnam}_resized.${format}`);
+      console.log(imgPath);
       res.set('Content-Type', `image/${format}`);
       return res.status(200).sendFile(imgPath);
       // return res.status(200).download(imgPath, `resized thumbnail_${imgnam}.${format}`);
     })
-    .catch(err => res.status(400).json({ error: 'The URL specified is not an image' }));
+    .catch((error) => {
+      if (fs.existsSync(imgPath)) {
+        console.log(imgPath);
+        console.log(error);
+        console.log(`Code is ${error.code}`);
+        console.log(`Message is ${error.message}`);
+        return res.status(500).json({ error: 'Error occured while sending image to user' });
+      }
+
+      if (error.code === 'ENOTFOUND' || error.code === 'ECONNRESET' || error.code === 'ETIMEDOUT') {
+        console.log(imgPath);
+        console.log(error);
+        console.log(`Code is ${error.code}`);
+        console.log(`Message is ${error.message}`);
+        return res.status(500).json({ error: 'Network connection error. Unable to access image URL' });
+      }
+
+      console.log(imgPath);
+      console.log(error);
+      console.log(`Code is ${error.code}`);
+      console.log(`Message is ${error.message}`);
+
+      return res.status(400).json({ error: 'The URL specified is not an image' });
+    });
 
   /* request.get(url)
     .pipe(fs.createWriteStream(`image_${imgnam}.${req.body.format}`))
